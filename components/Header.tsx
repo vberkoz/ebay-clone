861e22d2fd13ecc5af3755ff0c7b940467752626
vberkoz <vberkoz@vf-soft.com>
2022-11-17 00:39:20 +0200

    Implementing MetaMask Authentication

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,24 @@
 import React from "react";
+import { useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react";
 
 type Props = {};
 
 function Header({}: Props) {
-  return <div>
-    <nav>
-      <button className="connectWalletBtn">Connect your wallet</button>
-    </nav>
-  </div>;
+  const connectWithMetaMask = useMetamask();
+  const disconnect = useDisconnect();
+  const address = useAddress();
+
+  return (
+    <div>
+      <nav>
+        {address ? (
+          <button onClick={disconnect} className="connectWalletBtn">Hi, {address.slice(0, 5) + "..." + address.slice(-4)}</button>
+        ) : (
+          <button onClick={connectWithMetaMask} className="connectWalletBtn">Connect your wallet</button>
+        )}
+      </nav>
+    </div>
+  );
 }
 
 export default Header;
